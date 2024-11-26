e95325b1a95ff1f5cfea4fd3aba93d99f0f09afe
Werner Kühn <werner_kuehn@gmx.de>
2024-11-26 16:25:16 +0100

    resolve migration using index_together

--- django_cron/migrations/0001_initial.py
+++ django_cron/migrations/0001_initial.py
@@ -1,47 +1,37 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.db import models, migrations
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
 
-    dependencies = []
+    initial = True
+
+    dependencies = [
+    ]
 
     operations = [
         migrations.CreateModel(
+            name='CronJobLock',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('job_name', models.CharField(max_length=200, unique=True)),
+                ('locked', models.BooleanField(default=False)),
+            ],
+        ),
+        migrations.CreateModel(
             name='CronJobLog',
             fields=[
-                (
-                    'id',
-                    models.AutoField(
-                        verbose_name='ID',
-                        serialize=False,
-                        auto_created=True,
-                        primary_key=True,
-                    ),
-                ),
-                ('code', models.CharField(max_length=64, db_index=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('code', models.CharField(db_index=True, max_length=64)),
                 ('start_time', models.DateTimeField(db_index=True)),
                 ('end_time', models.DateTimeField(db_index=True)),
                 ('is_success', models.BooleanField(default=False)),
-                ('message', models.TextField(max_length=1000, blank=True)),
-                (
-                    'ran_at_time',
-                    models.TimeField(
-                        db_index=True, null=True, editable=False, blank=True
-                    ),
-                ),
+                ('message', models.TextField(blank=True, default='')),
+                ('ran_at_time', models.TimeField(blank=True, db_index=True, editable=False, null=True)),
             ],
-        ),
-        migrations.AlterIndexTogether(
-            name='cronjoblog',
-            index_together=set(
-                [
-                    ('code', 'is_success', 'ran_at_time'),
-                    ('code', 'start_time', 'ran_at_time'),
-                    ('code', 'start_time'),
-                ]
-            ),
+            options={
+                'indexes': [models.Index(fields=['code', 'is_success', 'ran_at_time'], name='django_cron_code_89ad04_idx'),
+                            models.Index(fields=['code', 'start_time', 'ran_at_time'], name='django_cron_code_21f381_idx'),
+                            models.Index(fields=['code', 'start_time'], name='django_cron_code_966ed3_idx')],
+            },
         ),
     ]
